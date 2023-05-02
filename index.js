a86ffd9d6d55338d74bb9c786a1c4b97fb51d7d1
bzaman_tk <bzaman.tk@gmail.com>
2023-05-02 11:28:03 +0600

    data added

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 5000
+const chefs = require('./data/chefs.json')
+const recipe = require('./data/recipe.json')
+
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('<h1>Server Is Working Fine</h1>')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+})
+
+app.get('/recipe', (req, res) => {
+    res.send(recipe)
 })
 
 app.listen(port, () => console.log('My port is: ', port))
